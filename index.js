7481334eea42f5f0bfc64515b8a94d03d2c0ac1c
skiprox <seanmichaelscanlan@gmail.com>
2018-11-14 17:46:26 -0500

    making just check for one coord function

--- index.js
+++ index.js
@@ -12,13 +12,6 @@ const say = require('say');
 /**
  * Variables
  */
-let xCoordinates = coordinates.map(coord => coord.x);
-let yCoordinates = coordinates.map(coord => coord.y);
-let xMin = Math.min(...xCoordinates);
-let xMax = Math.max(...xCoordinates);
-let yMin = Math.min(...yCoordinates);
-let yMax = Math.max(...yCoordinates);
-const timeInterval = 10;
 const weatherRequestOptions = {
 	url: 'http://api.worldweatheronline.com/premium/v1/weather.ashx',
 	qs: {
@@ -27,11 +20,6 @@ const weatherRequestOptions = {
 		"format": "json"
 	}
 };
-const penURL = 'http://localhost:4242/v1/pen';
-const penHeader = {
-	'Content-Type': 'application/json; charset=UTF-8'
-};
-let coordinatesIncrementer = 0;
 
 /**
  * APP
@@ -42,76 +30,20 @@ class App {
 	 * constructor [set everything up]
 	 */
 	constructor() {
-		this.sendRequest();
+		this.testPoint(8.35, 115.34);
 	}
 	/**
 	 * sendRequest [send a request to worldweatheronline]
 	 */
-	sendRequest() {
-		if (coordinatesIncrementer == coordinates.length) {
-			console.log('~~~~~~~~~~~~~~~ WE ARE EXITING ~~~~~~~~~~~~~~~');
-			say.speak('STOP');
-			process.exit();
-		}
-		let coordinate = coordinates[coordinatesIncrementer];
-		weatherRequestOptions.qs["q"] = `${coordinate.x},${coordinate.y}`;
-		coordinatesIncrementer++;
+	testPoint(lat, lng) {
+		weatherRequestOptions.qs["q"] = `${lat},${lng}`;
 		request(weatherRequestOptions, (error, response, body) => {
 			if (error) {
 				throw error;
 			} else {
 				let data = JSON.parse(body);
-				let currentCondition = data.data["current_condition"];
-				let cloudCover = parseFloat(currentCondition[0].cloudcover)/100;
-				this.draw(coordinate, cloudCover);
+				console.log(data.data);
 			}
-		});
-	}
-	/**
-	 * draw [draw on the axidraw]
-	 * @param  {String} coordinate [lat&lng coordinates in the form of e.g. '13.42,9.58']
-	 * @param  {Float} cloudCover [cloud cover, from 0 to 1 (drawn as 1 - cloudCover)]
-	 *
-	 * What we do here is send a request to localhost to reset the state of the pen
-	 * to 0 (UP), then send a request to move it to the appropriate coordinates,
-	 * then send a request to set the state of the pen to whatever the cloud coverage is
-	 */
-	draw(coordinate, cloudCover) {
-		// TODO
-		// put this into promises
-		let xCoord = coordinate.x;
-		let yCoord = coordinate.y;
-		let percentX = utils.mapClamp(xCoord, xMin, xMax, 25, 75);
-		let percentY = utils.mapClamp(yCoord, yMin, yMax, 25, 75);
-		request.put(penURL, {
-			headers: penHeader,
-			body: JSON.stringify({
-				"state": 0
-			})
-		}, (error, response, body) => {
-			if (error) throw error;
-			request.put(penURL, {
-				headers: penHeader,
-				body: JSON.stringify({
-					"x": percentX,
-					"y": percentY
-				})
-			}, (error, response, body) => {
-				if (error) throw error;
-				request.put(penURL, {
-					headers: penHeader,
-					body: JSON.stringify({
-						"state": 1 - cloudCover
-					})
-				}, (error, response, body) => {
-					if (error) throw error;
-					console.log(response.body);
-					setTimeout(() => {
-						console.log('we send another request');
-						this.sendRequest();
-					}, timeInterval * 1000);
-				});
-			});
 		});
 	}
 }
